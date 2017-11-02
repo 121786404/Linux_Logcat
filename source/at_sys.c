33e0de51c0391e8736041ecefd42a2bd37788022
sheldon.liu <sheldon.liu@mstarsemi.com>
2017-11-02 10:58:42 +0800

    delete constructor attr

--- source/at_sys.c
+++ source/at_sys.c
@@ -2,7 +2,7 @@
 
 
 extern int __system_properties_init(void);
-void __attribute__((constructor)) at_sys_init(void)
+void at_sys_init(void)
 {
 	__system_properties_init();
 //	printf("__system_properties_init=%d\n",res);
